3adbda45f5f523d89961d4c6e47664d77cc32d01
Vibhas Bhingarde <vibhasb@hdfclife.com>
2022-08-05 20:56:48 +0530

    Setting up code pipeline

--- bin/ecs-cdk-ci-cd.ts
+++ bin/ecs-cdk-ci-cd.ts
@@ -2,20 +2,17 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { EcsCdkCiCdStack } from '../lib/ecs-cdk-ci-cd-stack';
+import { Tags } from 'aws-cdk-lib';
 
 const app = new cdk.App();
-new EcsCdkCiCdStack(app, 'EcsCdkCiCdStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const projectName = process.env.PROJECT_NAME || app.node.tryGetContext('projectname');
+const stage = process.env.DEPLOYMENT_ENV || app.node.tryGetContext('env');
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const myStack = new EcsCdkCiCdStack(app, `${projectName}-${stage}`, {
+  stackName: `${projectName}-${stage}`,
+  env: { region: 'ap-south-1' },
 });
+
+Tags.of(myStack).add('projectname', `${projectName}`);
+Tags.of(myStack).add('env', `${stage}`);
--- lib/ecs-cdk-ci-cd-stack.ts
+++ lib/ecs-cdk-ci-cd-stack.ts
@@ -1,16 +1,57 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { Duration, SecretValue, Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import { CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { BuildSpec } from 'aws-cdk-lib/aws-codebuild';
 
 export class EcsCdkCiCdStack extends Stack {
+  public readonly projectName: string = this.node.tryGetContext('projectname');
+  public readonly deploymentStage: string = this.node.tryGetContext('env');
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const pipeline = new codepipeline.Pipeline(this, `${this.projectName}-${this.deploymentStage}`, {
+      pipelineName: `${this.projectName}-${this.deploymentStage}`,
+      crossAccountKeys: false,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EcsCdkCiCdQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const gitHubSourceArtifacts = new codepipeline.Artifact();
+
+    const gitHubSourceAction = new GitHubSourceAction({
+      actionName: 'DownloadSourceCode',
+      owner: 'vibhasfl',
+      repo: 'cicddemoapps',
+      oauthToken: SecretValue.secretsManager('github', { jsonField: 'githuboauthtoken' }),
+      branch: 'dev',
+      output: gitHubSourceArtifacts,
+    });
+
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [gitHubSourceAction],
+    });
+
+    const buildStageArtifacts = new codepipeline.Artifact();
+
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new CodeBuildAction({
+          actionName: 'InstallDependencies',
+          input: gitHubSourceArtifacts,
+          outputs: [buildStageArtifacts],
+          project: new codebuild.PipelineProject(this, `${this.projectName}-codebld-${this.deploymentStage}`, {
+            buildSpec: BuildSpec.fromSourceFilename('codebuild.yml'),
+            environment: {
+              buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+              computeType: codebuild.ComputeType.SMALL,
+            },
+            timeout: Duration.minutes(5),
+          }),
+        }),
+      ],
+    });
   }
 }
